0b2b9dc8e5ad200e2768d191f1aa704bf405585f
Georgios Kaiafas <kfsgiorgos@gmail.com>
2022-02-09 07:54:20 +0100

    map with 2 iterables

--- python-is-cool.py
+++ python-is-cool.py
@@ -20,4 +20,22 @@ nums_squared_2 = map(lambda x: x * x, nums)
 
 assert list(nums_squared_1) == nums_squared
 
+# We use map with more than one iterable.
+# We calculate the mean squared error of a simple linear function f(x) = ax + b using the true label labels
+a, b = 3, -0.5
+xs = [2, 3, 4, 5]
+labels = [6.4, 8.9, 10.9, 15.3]
+
+# Method 1: using a loop
+errors = []
+for i, x in enumerate(xs):
+    errors.append((a * x + b - labels[i]) ** 2)
+result1 = sum(errors) ** 0.5 / len(xs)
+
+# Method 2: using map
+diffs = map(lambda x, y: (a * x + b - y) ** 2, xs, labels)
+result2 = sum(diffs) ** 0.5 / len(xs)
+
+assert result1 == result2, "result1 != result2"
+
 
