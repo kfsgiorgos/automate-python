d49085b9364718623cda1131ed6adbb2bb105ab9
Georgios Kaiafas <kfsgiorgos@gmail.com>
2022-02-11 21:55:59 +0100

    remove duplicates O(N) complexity

---
+++ remove-duplicates-array.py
@@ -0,0 +1,11 @@
+def removeDuplicates(nums):
+    l = 1
+
+    for r in range(1, len(nums)):
+        if nums[r] != nums[r-1]:
+            nums[l] = nums[r]
+            l += 1
+    return nums[:l]
+
+
+print(removeDuplicates([1,1,2]))
