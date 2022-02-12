1c3edf63e3cc4be9229f7041d2aa514b13bf8101
Georgios Kaiafas <kfsgiorgos@gmail.com>
2022-02-12 11:06:51 +0100

    two sum array

---
+++ two-sum-array.py
@@ -0,0 +1,13 @@
+# We are going to solve Leetcode 1 - Given a target value (sum) can we find a pair in the array that sums to the target?
+
+def twoSum(nums, target:int):
+    prevMap = {} # val : index
+
+    for i, arraynumber in enumerate(nums):
+        diff = target - arraynumber
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[arraynumber] = i
+
+
+print(twoSum([2,7,11,15], 18))
