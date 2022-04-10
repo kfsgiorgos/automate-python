881dc6fea2db783031966816f0ded4bff170e095
Georgios Kaiafas <kfsgiorgos@gmail.com>
2022-04-10 21:12:12 +0200

    read txt file

--- python-command-line.py
+++ python-command-line.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import shutil
 from pprint import pprint
+import pylint
 
 # Get your current working directly
 # This returns a string
@@ -20,3 +21,14 @@ print("\nRead Write files")
 file = open("/Users/georgios.kaiafas/Desktop/test_file.txt","r")
 print(f"4th line of the txt file is: {file.read(3)}")
 file.close()
+
+f = open("/Users/georgios.kaiafas/Desktop/test_write.txt",'r',encoding = 'utf-8')
+print("read the first 4 data")
+f.read(4)  
+
+print("read the next 4 data")
+f.read(4)    
+print("read in the rest till end of file")
+f.read()   
+print("further reading returns empty sting")
+f.read()  
---
+++ supporting.py
@@ -0,0 +1,3 @@
+# Initialize values to be used in another script
+vacation1 = "Summer Vacation"
+vacation2 = "Winter Vacation"
