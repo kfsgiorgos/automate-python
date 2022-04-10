fc845032d5f11f87370cb3b1347b148c1b7bcf1c
Georgios Kaiafas <kfsgiorgos@gmail.com>
2022-04-10 21:11:58 +0200

    fix identation

--- create-new-txt.py
+++ create-new-txt.py
@@ -1,5 +1,4 @@
-print("\nCreate a new txt file")
-with open("test_write.txt",'w',encoding = 'utf-8') as f:
-   f.write("my first file using Python\n")
-   f.write("This file has used Python scripting\n\n")
-   f.write("and it contains just three lines of text.\n")
+with open("/Users/georgios.kaiafas/Desktop/test_write.txt",'w',encoding = 'utf-8') as f:
+    f.write("my first file using Python\n")
+    f.write("This file has used Python scripting\n\n")
+    f.write("and it contains just three lines of text.\n")
